bae2cf2b2e5bd987fb1f8d0a611941b7bddd724d
Mike <mikeper030@gmail.com>
2018-10-17 00:00:27 +0300

    first header file

--- ex1/Rectangle.h
+++ ex1/Rectangle.h
@@ -4,7 +4,20 @@ class Rectangle
 {
 public:
 	Rectangle();
+	Rectangle(const Vertex veritces[2]);
 	Rectangle(const Vertex& bottomLeft, const Vertex& topRight);
+	Rectangle(double x0, double y0, double x1, double y1);
+	Rectangle(const Vertex& start, double width, double height);
 	~Rectangle();
+	Vertex getBottomLeft() const;
+	Vertex getTopRight()const;
+	Vertex getWidth() const;
+	Vertex getHeight() const;
+
+
+  
+private:
+	void verifyInput();
+
 };
 
