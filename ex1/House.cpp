96647c68b501b84f6c1ef72f7e0f51a5303f2c3e
Vizan Meir <vizanme@std.hac.ac.il>
2018-10-21 19:51:43 +0300

    new update house and kite

--- ex1/House.cpp
+++ ex1/House.cpp
@@ -4,15 +4,33 @@
 
 House::House()
 {
+	m_rect[0] = { 20,10 };
+	m_rect[1] = { 30,20 };
+	m_triag[0] = { 20,20 };
+	m_triag[1] = { 25,25 };
+	m_triag[2] = { 30,20 };
 }
 
 House::House(const Rectangle & rectangle, const IsoscelesTriangle & triangle)
+	:House()
 {
-
+	if (rectangle.getTopRight().m_y == triangle.getVertex(2).m_y 
+		|| triangle.getVertex(0).m_y == rectangle.getTopRight().m_y
+		|| rectangle.getBottomLeft().m_x >= triangle.getVertex(0).m_x
+		|| rectangle.getTopRight().m_x <= triangle.getVertex(2).m_x)
+	{
+		m_rect[0] = rectangle.getBottomLeft();
+		m_rect[1] = rectangle.getTopRight();
+		m_triag[0] = triangle.getVertex(0);
+		m_triag[1] = triangle.getVertex(1);
+		m_triag[2] = triangle.getVertex(2);
+	}
 }
+
 
 House::House(const Vertex & roofTop, double width, double roofHeight, double baseHeight)
 {
+
 }
 
 bool House::extendRoof(double width)
@@ -34,3 +52,4 @@ double House::getWidthDifferent() const
 House::~House()
 {
 }
+
